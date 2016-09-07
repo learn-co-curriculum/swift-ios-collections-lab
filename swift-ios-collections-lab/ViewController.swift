771f2698bc59260de0c4311444da72c0040a25e0
Johann Kerr <johannkerr@gmail.com>
2016-09-06 21:46:33 -0400

    Initial Commit

---
+++ swift-ios-collections-lab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-ios-collections-lab
+//
+//  Created by Johann Kerr on 9/6/16.
+//  Copyright © 2016 Johann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
